4acd93015caf4f4bf1202e84b90e2e8ec151bee6
literally <literally_user@hotmail.com>
2025-11-27 00:57:55 +0300

    add: pic for hardware access

--- rimmy_kernel/src/lib.rs
+++ rimmy_kernel/src/lib.rs
@@ -10,5 +10,8 @@ use crate::framebuffer::{init_framebuffer, init_writer};
 pub fn init(fb: &Framebuffer) {
     init_framebuffer(fb);
     init_writer();
-    arch::x86_64::idt::init_idt();
+    arch::x86_64::gdt::init();
+    arch::x86_64::idt::init();
+    // unsafe { arch::x86_64::idt::PICS.lock().initialize() };
+    // x86_64::instructions::interrupts::enable();
 }
