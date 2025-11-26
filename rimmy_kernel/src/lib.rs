c93495eb7d7f9ed0932eb51af2e9eb8621f2a8b5
literally <literally_user@hotmail.com>
2025-11-27 00:53:09 +0300

    basic idt support

--- rimmy_kernel/src/lib.rs
+++ rimmy_kernel/src/lib.rs
@@ -1,6 +1,8 @@
+#![feature(abi_x86_interrupt)]
 #![no_std]
 
 pub mod framebuffer;
+pub mod arch;
 
 use limine::framebuffer::Framebuffer;
 use crate::framebuffer::{init_framebuffer, init_writer};
@@ -8,4 +10,5 @@ use crate::framebuffer::{init_framebuffer, init_writer};
 pub fn init(fb: &Framebuffer) {
     init_framebuffer(fb);
     init_writer();
+    arch::x86_64::idt::init_idt();
 }
