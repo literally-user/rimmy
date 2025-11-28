e262cbfcc860333f5195b6609d2b27f14769b737
literally <literally_user@hotmail.com>
2025-11-28 17:30:26 +0300

    add: page fault and general protection fault

--- rimmy_kernel/src/lib.rs
+++ rimmy_kernel/src/lib.rs
@@ -10,7 +10,7 @@ use crate::framebuffer::{init_framebuffer, init_writer};
 pub fn init(fb: &Framebuffer) {
     init_framebuffer(fb);
     init_writer();
-    // arch::x86_64::gdt::init();
+    arch::x86_64::gdt::init();
     arch::x86_64::idt::init();
     arch::x86_64::idt::init_pics();
     x86_64::instructions::interrupts::enable();
