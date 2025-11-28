6d144edf2c2248f33d97c8dca7e920248202528c
literally <literally_user@hotmail.com>
2025-11-28 18:37:05 +0300

    fix: race condition and unmapped key press

--- rimmy_kernel/src/framebuffer/mod.rs
+++ rimmy_kernel/src/framebuffer/mod.rs
@@ -91,5 +91,9 @@ macro_rules! println {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    get_writer().write_fmt(args).unwrap();
+    use x86_64::instructions::interrupts;
+    
+    interrupts::without_interrupts(|| {
+        get_writer().write_fmt(args).unwrap();
+    });
 }
